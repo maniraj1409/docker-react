767a88bdeb739f167a1d96cb7bee6b7bc7a58db2
Venkata Maniraj <venkatama@INLM-ManirajSV.local>
2020-06-27 15:02:09 +0530

    changed the source code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi Maniraj, ur project is ready!
         </p>
         <a
           className="App-link"
